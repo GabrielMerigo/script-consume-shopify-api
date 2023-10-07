155ac0f9454c80f0c5dc9c70f17dcae98d376180
JoaoVitorLima242 <jlima0500@gmail.com>
2023-10-06 23:13:43 -0300

    testing husky

--- src/env/index.ts
+++ src/env/index.ts
@@ -10,7 +10,7 @@ const _env = envSchema.safeParse(process.env);
 
 if (_env.success === false) {
   console.error('⚠️ Invalid environment variables', _env.error.format());
-  throw new Error('Invalid environment variables.');
+  throw new Error("Invalid environment variables.");
 }
 
 export const env = _env.data;
