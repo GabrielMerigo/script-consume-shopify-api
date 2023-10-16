f554e26acdd2a7f07f8d97d03385ca45ae91b6ad
Gabriel <gabriel.merigo.dev@gmail.com>
2023-10-15 21:46:45 -0300

    adding test config, first test and husky into husky config

---
+++ test/utils/resizeImage.spec.ts
@@ -0,0 +1,8 @@
+import { expect, it } from 'vitest';
+import { resizeImage } from '../../src/utils';
+
+it('should be able to replace /fit-in/600x600 to /fit-in/1000x1000 of a string', () => {
+  const resizedValue = resizeImage('/fit-in/600x600');
+
+  expect(resizedValue).include('1000x1000');
+});
