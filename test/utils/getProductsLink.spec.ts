f99674a3b3181253d124159ff7919be6f7fe9cf1
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-10-19 18:47:09 -0300

    feat: adding test for getProductsLink (#12)

--- test/utils/getProductsLink.spec.ts
+++ test/utils/getProductsLink.spec.ts
@@ -1,1 +1,17 @@
-it('should return the link of a page', async () => {});
+import { Page } from 'puppeteer';
+import { getProductsLink } from '../../src/utils';
+
+const evaluateFunc = jest.fn();
+
+const page: Pick<Page, 'evaluate'> = {
+  evaluate: evaluateFunc.mockReturnValue(['link1', 'link2'])
+};
+
+describe('getProductsLink', () => {
+  it('should return the links from page correctly', async () => {
+    const productsLink = await getProductsLink(page as Page);
+
+    expect(productsLink).toEqual(['link1', 'link2']);
+    expect(page.evaluate).toHaveBeenCalledWith(expect.any(Function));
+  });
+});
