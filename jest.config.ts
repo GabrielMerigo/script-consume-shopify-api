129f0c059aaa29a0e769b5bd6baa9587e31ae883
JoaoVitorLima242 <jlima0500@gmail.com>
2023-11-02 19:38:55 -0300

    feat: making tests pass

--- jest.config.ts
+++ jest.config.ts
@@ -3,18 +3,21 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   verbose: true,
-  transformIgnorePatterns: ['/node_modules/'],
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
+  transformIgnorePatterns: ['/node_modules/'],
+  testMatch: ['<rootDir>/test/**/*.spec.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
   moduleNameMapper: {
-    '@constants': ['../src/constants/index'],
-    '@data': ['../../src/data/index'],
-    '@env': ['../../src/env/index'],
-    '@mocks': ['../../src/mocks/index'],
-    '@requests/*': ['../../src/requests/*'],
-    '@services/*': ['../../src/services/*'],
-    '@types': ['../../src/types/index'],
-    '@utils': ['../../src/utils/index']
+    '@constants': ['<rootDir>/src/constants'],
+    '@data': ['<rootDir>/src/data'],
+    '@env': ['<rootDir>/src/env'],
+    '@mocks': ['<rootDir>/src/mocks'],
+    '@requests/(.*)$': ['<rootDir>/src/requests/$1'],
+    '@services/(.*)$': ['<rootDir>/src/services/$1'],
+    '@types': ['<rootDir>/src/types'],
+    '@utils': ['<rootDir>/src/utils']
   }
 };
 
