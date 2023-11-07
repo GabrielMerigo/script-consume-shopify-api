48c819bdba7ea8f4ee61d6b968f87487cc72a4f6
JoaoVitorLima242 <jlima0500@gmail.com>
2023-11-06 23:21:39 -0300

    feat: removing console

--- src/utils/addProductsByAllCollections.ts
+++ src/utils/addProductsByAllCollections.ts
@@ -3,8 +3,6 @@ import { createProducts } from '../index';
 
 export const addProductsByAllCollections = (): void => {
   COLLECTIONS.forEach((collection) => {
-    console.log(`Starting ${collection} create product process`);
     createProducts(collection);
-    console.log(`Finishing ${collection} create product process`);
   });
 };
