7f079d7cf7bb021518d7d2f45aadf4b93c05393b
JoaoVitorLima242 <jlima0500@gmail.com>
2023-11-06 23:19:32 -0300

    feat: adding console log

--- src/utils/addProductsByAllCollections.ts
+++ src/utils/addProductsByAllCollections.ts
@@ -3,6 +3,8 @@ import { createProducts } from '../index';
 
 export const addProductsByAllCollections = (): void => {
   COLLECTIONS.forEach((collection) => {
+    console.log(`Starting ${collection} create product process`);
     createProducts(collection);
+    console.log(`Finishing ${collection} create product process`);
   });
 };
