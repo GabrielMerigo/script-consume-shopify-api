47712113805746da7ab5f403487805464b900012
JoaoVitorLima242 <jlima0500@gmail.com>
2023-11-06 21:24:58 -0300

    feat: adding more complex tests

--- test/utils/addProductsByAllCollections.spec.ts
+++ test/utils/addProductsByAllCollections.spec.ts
@@ -1,14 +1,21 @@
-import { BaseCollections } from "@types"
-import { addProductsByAllCollections } from "@utils"
-import { createProducts } from '../../src/index'
+import { BaseCollections } from '@types';
+import { addProductsByAllCollections } from '@utils';
+import { createProducts } from '../../src/index';
 
 jest.mock('../../src/index', () => ({
-  createProducts: jest.fn(),
+  createProducts: jest.fn()
 }));
 
 describe('addProductsByAllCollections', () => {
-  it(`should call createProducts ${BaseCollections.length}`, () => {
+  it(`should call createProducts ${BaseCollections.length} times`, () => {
     addProductsByAllCollections();
     expect(createProducts).toHaveBeenCalledTimes(BaseCollections.length);
+  });
+
+  BaseCollections.forEach((collection) => {
+    it(`should call the createProducts function with ${collection} as a param`, () => {
+      addProductsByAllCollections();
+      expect(createProducts).toHaveBeenCalledWith(collection);
+    });
   });
 });
