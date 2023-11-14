d273e552d0f26ae7031ceb9f586896b203b6193c
JoaoVitorLima242 <jlima0500@gmail.com>
2023-11-13 23:06:12 -0300

    fix: fixing logic error

--- src/utils/productAlreadyExistsInShopify.ts
+++ src/utils/productAlreadyExistsInShopify.ts
@@ -4,14 +4,8 @@ export const productAlreadyExistsInShopify = (
   product: ProductToInsertIntoShopify,
   shopifyProducts: ShopifyProduct[]
 ): ShopifyProduct | null => {
-  if (!product.variants.length) {
-    console.log('ERROR: Product without variants' + product.title);
-
-    return null;
-  }
-
   const foundProduct = shopifyProducts.find(
-    (i) => i.variants[0]?.sku == product.variants[0]?.sku
+    (i) => i?.variants[0]?.sku == product?.variants[0]?.sku
   );
 
   return foundProduct || null;
