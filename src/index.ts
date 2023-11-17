b073824a7434ee997d53fb2c62ff7620a42c0a0b
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-11-17 18:16:05 -0300

    fix: switching 20 to 00

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import cron from 'node-cron';
 import { createProductsByAllCollections } from 'processes/createProductsByAllCollections'
 
-const everyDayAt8amAnd8pm = '0 8,20 * * *';
+const everyDayAt8amAnd8pm = '0 8,00 * * *';
 
 cron.schedule(everyDayAt8amAnd8pm, () => {
   createProductsByAllCollections();
