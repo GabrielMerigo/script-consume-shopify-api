75ecd106a2783c45fe2d199b02dc900f520b865c
JoaoVitorLima242 <jlima0500@gmail.com>
2023-12-09 19:11:52 -0300

    test: testing cron

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import cron from 'node-cron';
 import { addProductsByAllCollections } from '@utils';
 
-const everyDayAt8amNoon2pm5pmAnd9pm = '0 8,12,14,17,19,21 * * *';
+// const everyDayAt8amNoon2pm5pmAnd9pm = '0 8,12,14,17,19,21 * * *';
 
-cron.schedule(everyDayAt8amNoon2pm5pmAnd9pm, () => {
+addProductsByAllCollections();
+cron.schedule('18 19 * * *', () => {
   addProductsByAllCollections();
 });
