49ef0d4fb035bde4fbd6af421e57eb355ff32f16
JoaoVitorLima242 <jlima0500@gmail.com>
2023-12-09 19:14:00 -0300

    test: testing cron

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { addProductsByAllCollections } from '@utils';
 
 // const everyDayAt8amNoon2pm5pmAnd9pm = '0 8,12,14,17,19,21 * * *';
 
-addProductsByAllCollections();
 cron.schedule('18 19 * * *', () => {
   addProductsByAllCollections();
 });
