9c7ba57046f7044b3cbce81ef6ddf58856ab1aa0
JoaoVitorLima242 <jlima0500@gmail.com>
2023-12-09 19:05:33 -0300

    feat: going back to happy place :)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-// import cron from 'node-cron';
+import cron from 'node-cron';
 import { addProductsByAllCollections } from '@utils';
 
-// const everyDayAt8amAnd12am = '0 8,00 * * *';
+const everyDayAt8amNoon2pm5pmAnd9pm = '0 8,12,14,17,19,21 * * *';
 
-// cron.schedule(everyDayAt8amAnd12am, () => {
-addProductsByAllCollections();
-// });
+cron.schedule(everyDayAt8amNoon2pm5pmAnd9pm, () => {
+  addProductsByAllCollections();
+});
