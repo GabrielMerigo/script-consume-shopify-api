4fa092c3f1c6a5c6a1e3dd244c21ef1323e50ccf
JoaoVitorLima242 <jlima0500@gmail.com>
2023-12-09 18:40:05 -0300

    test: test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import cron from 'node-cron';
+// import cron from 'node-cron';
 import { addProductsByAllCollections } from '@utils';
 
-const everyDayAt8amAnd12am = '0 8,00 * * *';
+// const everyDayAt8amAnd12am = '0 8,00 * * *';
 
-cron.schedule(everyDayAt8amAnd12am, () => {
-  addProductsByAllCollections();
-});
+// cron.schedule(everyDayAt8amAnd12am, () => {
+addProductsByAllCollections();
+// });
--- src/utils/getProductsInformationBasedOnUrl.ts
+++ src/utils/getProductsInformationBasedOnUrl.ts
@@ -11,7 +11,7 @@ export const getProductsInformationBasedOnUrl = async ({
 }> => {
   const puppeteerLaunchOptions: PuppeteerLaunchOptions = {
     headless: 'new',
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: ['--no-sandbox']
   };
 
   const browser = await puppeteer.launch(puppeteerLaunchOptions);
